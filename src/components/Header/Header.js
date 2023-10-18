b656b678508e869a4991265a995166913e716d17
JoseJ55 <josejimenez826@gmail.com>
2023-10-17 22:23:25 -0700

    added new header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,12 +1,30 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./style.css";
 
-import Navbar from "../Navbar/Navbar";
-
 function Header() {
+    const [scrollPosition, setScrollPosition] = useState(0);
+
+    useEffect(() => {
+        const handleScroll = () => {
+            const currentPosition = window.scrollY;
+            setScrollPosition(currentPosition);
+        }
+
+        window.addEventListener('scroll', handleScroll);
+
+        return () => {
+            window.removeEventListener('scroll', handleScroll);
+        }
+    }, []);
+
     return (
         <div id="header">
-            <Navbar />
+            <img className='header-background' src='/images/Layered/drink-1-background.png' alt='first layer background for header' />
+            <img className='header-forground' style={{ transform: `translateY(${Math.min(scrollPosition / 700, 5)}%)`}} src='/images/Layered/drink-1-forground.png' alt='second layer background for header' />
+        
+            <div id='header-info-container'>
+                <p>Experience Seamless Dining & Drinking With <span className='title-text'>Thirsty</span></p>
+            </div>
         </div>
     )
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,10 +5,12 @@ import Header from "./../components/Header/Header";
 import Popular from "../components/Popular/Popular";
 import Drinks from "../components/Drinks/Drinks";
 import About from "../components/About/About";
+import Navbar from "../components/Navbar/Navbar";
 
 function Home() {
     return (
         <div>
+            <Navbar />
             <Header />
             <Popular /> 
             <Drinks />
