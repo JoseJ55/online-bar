38a3ca840c0215174dddb2033b38548de78252fe
JoseJ55 <josejimenez826@gmail.com>
2024-01-14 16:46:48 -0800

    added responsive code to menu and app section

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -6,7 +6,7 @@ function App() {
     <div id='app'>
         <div id='app-info'>
             <p>Seamless Ordering with the <span id='app-name'>Thirsty</span> App</p>
-            <p>Explore a diverse range of culinary delights and tantalizing drinks from the comfort of your phone.</p>
+            <p id='app-sub-text'>Explore a diverse range of culinary delights and tantalizing drinks from the comfort of your phone.</p>
             
             <div id='app-info-icons'>
                 <img src='/images/google-play-icon.png' alt='icon for google play' />
